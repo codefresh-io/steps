e743c629b86f49183af1aa182faec337ac2ffd99
ziv-codefresh <ziv@codefresh.io>
2019-08-14 18:16:44 +0300

    update github-release-plugin

--- incubating/github-release/run.sh
+++ incubating/github-release/run.sh
@@ -8,7 +8,7 @@ done
 if [ "$PRERELEASE" = "true" ]; then PRERELEASE="-p"; else PRERELEASE=""; fi
 if [ "$CF_TARGET_BRANCH" ]; then CF_TARGET_BRANCH="--target $CF_TARGET_BRANCH"; fi
 
-github-release release --user $CF_REPO_OWNER --repo $CF_REPO_NAME --tag ziv22 --name ziv22
+github-release release --user $CF_REPO_OWNER --repo $CF_REPO_NAME --tag $RELEASE_TAG --name $RELEASE_NAME --description $RELEASE_DESCRIPTION
 
 # if [ ! -z "$FILES" ]; then
 #     for file in $FILES; do

5919986149e0cac065bbf385a910aaae5bf971cd
kuchuk-codefresh <r.kuchuk@codefresh.io>
2019-12-04 18:21:29 +0300

    SAAS-4266: implement support of jenkins build parameters

--- incubating/run-jenkins-jobs/cfjenkins.go
+++ incubating/run-jenkins-jobs/cfjenkins.go
@@ -15,36 +15,49 @@ import (
 type (
 	// Jenkins Job parameters
 	JenkinsJobParams struct {
-		Username  string
-		Token     string
-		Host      string
-		Job       string
-		Jobparams string
+		Username         string
+		Token            string
+		Host             string
+		Job              string
+		Buildparams      string
+		Parameterizedjob string
 	}
 )
 
 func main() {
-
 	host := os.Getenv("jenkins_url")
 	token := os.Getenv("jenkins_token")
 	user := os.Getenv("jenkins_username")
 	job := os.Getenv("jenkins_job_name")
-	jobparams := ""
-
-	jenkins := NewJenkinsJobParams(user, token, host, job, jobparams)
+	buildParams := GetBuildParams()
+	parameterizedJob := os.Getenv("jenkins_parameterized_job")
+	fmt.Println(buildParams)
+	fmt.Println(parameterizedJob)
+	jenkins := NewJenkinsJobParams(user, token, host, job, buildParams, parameterizedJob)
 	jenkins.trigger()
-
 }
 
-func NewJenkinsJobParams(user string, token string, host string, job string, jobparams string) *JenkinsJobParams {
-	host = strings.TrimRight(host, "/")
+func GetBuildParams() string {
+	Values := url.Values{}
+	for _, line := range os.Environ() {
+		if strings.HasPrefix(line, "build_param_") {
+			s := strings.TrimPrefix(line, "build_param_")
+			pair := strings.SplitN(s, "=", 2)
+			Values.Add(pair[0], pair[1])
+		}
+	}
+	return Values.Encode()
+}
 
+func NewJenkinsJobParams(user string, token string, host string, job string, buildParams string, parameterizedJob string) *JenkinsJobParams {
+	host = strings.TrimRight(host, "/")
 	return &JenkinsJobParams{
-		Username:  user,
-		Token:     token,
-		Host:      host,
-		Job:       job,
-		Jobparams: jobparams,
+		Username:         user,
+		Token:            token,
+		Host:             host,
+		Job:              job,
+		Buildparams:      buildParams,
+		Parameterizedjob: parameterizedJob,
 	}
 }
 
@@ -52,49 +65,53 @@ func (jenkins *JenkinsJobParams) trigger() {
 	if err := jenkins.validate(); err == false {
 		os.Exit(1)
 	}
-	path := fmt.Sprintf("%s/job/%s/%s", jenkins.Host, jenkins.Job, "build")
+
+	requestUrl := fmt.Sprintf("%s/job/%s/%s", jenkins.Host, jenkins.Job, "build")
+	if jenkins.Parameterizedjob == "true" {
+		if jenkins.Buildparams == "" {
+			log.Info(fmt.Sprintf("No build parameters provided!"))
+			log.Info(fmt.Sprintf("Continue with default parameters!"))
+			requestUrl = fmt.Sprintf("%s/job/%s/%s", jenkins.Host, jenkins.Job, "buildWithParameters")
+		} else {
+			log.Info(fmt.Sprintf("Parameters provided!"))
+			requestUrl = fmt.Sprintf("%s/job/%s/%s", jenkins.Host, jenkins.Job, "buildWithParameters"+"?"+jenkins.Buildparams)
+		}
+	}
 	log.Info(fmt.Sprintf("Going to trigger %s job on %s", jenkins.Job, jenkins.Host))
-	requestURL := jenkins.buildURL(path, url.Values{})
-	req, err := http.NewRequest("POST", requestURL, nil)
+
+	req, err := http.NewRequest("POST", requestUrl, nil)
 	if err != nil {
 		log.Error(err.Error())
 		os.Exit(1)
 	}
-
-	resp, err := jenkins.sendRequest(req)
+	req.SetBasicAuth(jenkins.Username, jenkins.Token)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Error(err)
 		os.Exit(1)
 	}
-	if resp.StatusCode == 404 {
-		log.Error(fmt.Sprintf("The trigger is failed! Job '%s' not found!", jenkins.Job))
+	switch resp.StatusCode {
+	case 404:
+		log.Error(fmt.Sprintf("The trigger is failed! Job \"%s\" not found", jenkins.Job))
 		os.Exit(1)
-	}
-	if resp.StatusCode == 201 || resp.StatusCode == 200 {
-		log.Info(resp.Status)
-		log.Info(fmt.Sprintf("The %s is triggered successfully", jenkins.Job))
-	} else {
+	case 200:
+		log.Info(fmt.Sprintf("Parameterized job \"%s\" is triggered successfully", jenkins.Job))
+		os.Exit(0)
+	case 201:
+		log.Info(fmt.Sprintf("Parameterized job \"%s\" is triggered successfully", jenkins.Job))
+		os.Exit(0)
+	case 400:
+		log.Error(fmt.Sprintf("The trigger is failed with status: %s", resp.Status))
+		log.Error(fmt.Sprintf("Seems like your jenkins project use parameters. Please set step argument 'jenkins_parameterized_job: true'"))
+		os.Exit(1)
+	case 500:
+		log.Error(fmt.Sprintf("The trigger is failed with status: %s", resp.Status))
+		log.Error(fmt.Sprintf("Seems like your jenkins project not use parameters. Please set step argument 'jenkins_parameterized_job: false'"))
+		log.Error(fmt.Sprintf("Or set parameterized flag in jenkins project settings"))
+	default:
 		log.Error(fmt.Sprintf("The trigger is failed with status: %s", resp.Status))
 		os.Exit(1)
 	}
-
-}
-
-func (jenkins *JenkinsJobParams) buildURL(path string, params url.Values) (requestURL string) {
-	requestURL = path
-	if params != nil {
-		queryString := params.Encode()
-		if queryString != "" {
-			requestURL = requestURL + "?" + queryString
-		}
-	}
-	return
-}
-
-func (jenkins *JenkinsJobParams) sendRequest(req *http.Request) (*http.Response, error) {
-
-	req.SetBasicAuth(jenkins.Username, jenkins.Token)
-	return http.DefaultClient.Do(req)
 }
 
 func (jenkins *JenkinsJobParams) parseResponse(resp *http.Response, body interface{}) (err error) {

e73677536df422a3cfa4a7a08cf5a31494c26841
Vasily Korytov <vasily@codefresh.io>
2020-05-14 15:51:49 +0300

    recursive submodules fix

--- incubating/git-submodules/script/updategitsubmodules.sh
+++ incubating/git-submodules/script/updategitsubmodules.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
+set -e
 
 if [ -z "$GITHUB_TOKEN" ]; then
     echo "ERROR: \$GITHUB_TOKEN var is not set or empty"
-    exit
+    exit 1
 fi
 
-mkdir ~/.ssh
-ssh-keyscan github.com > ~/.ssh/known_hosts
-sed -i 's/git@/https:\/\//' .gitmodules || exit
-sed -i 's/ssh:\/\///' .gitmodules
-sed -i 's/github.com:/github.com\//' .gitmodules
-sed -i "s/https:\/\/github.com/https:\/\/$GITHUB_TOKEN@github.com/" .gitmodules
+git config --global url.https://$GITHUB_TOKEN@github.com/.insteadOf git@github.com:
 
 if [ "$CF_SUBMODULE_SYNC" = "true" ]; then
-  echo "\$CF_SUBMODULE_SYNC var is set to 'true'. Syncing submodules"
-  echo "git submodule sync"
-  git submodule sync
+    echo "\$CF_SUBMODULE_SYNC var is set to 'true'. Syncing submodules"
+    echo "git submodule sync"
+    git submodule sync
 fi
 
 SUBMODULE_UPDATE_RECURSIVE_FLAG=""
 if [ "$CF_SUBMODULE_UPDATE_RECURSIVE" = "true" ]; then
-  echo "\$CF_SUBMODULE_UPDATE_RECURSIVE var is set to 'true'. Submodules will be recursively updated"
-  SUBMODULE_UPDATE_RECURSIVE_FLAG=--recursive
+    echo "\$CF_SUBMODULE_UPDATE_RECURSIVE var is set to 'true'. Submodules will be recursively updated"
+    SUBMODULE_UPDATE_RECURSIVE_FLAG=--recursive
 fi
 
 echo "Updating git submodules"

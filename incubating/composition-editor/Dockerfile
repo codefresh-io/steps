69a2579ea8b419882b0569007dc5a4df74e0fc69
Dustin Van Buskirk <dev@vanbuskirk.me>
2020-07-27 16:41:22 -0700

    commit wip

---
+++ incubating/composition-editor/Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.8.5-alpine3.12
+
+RUN pip install pyyaml
+
+COPY lib/composition-editor.py /composition-editor.py
---
+++ incubating/composition-editor/lib/composition-editor.py
@@ -0,0 +1,103 @@
+import os
+import yaml
+import sys
+
+def walkDict(dataDict, key, answer=None, sofar=None):
+    if sofar is None:
+        sofar = []
+    if answer is None:
+        answer = []
+    for k,v in dataDict.items():
+        if k == key:
+            answer.append(sofar + [k])
+        if isinstance(v, dict):
+            walkDict(v, key, answer, sofar+[k])
+    return answer
+
+
+# Used to delete ports not required for service to service communication
+def delKeysFromDict(dataDict, key):
+    for path in walkDict(dataDict, key):
+        dd = dataDict
+        while len(path) > 1:
+            dd = dd[path[0]]
+            path.pop(0)
+        dd.pop(path[0])
+
+
+def getFromDict(dataDict, mapList):
+    for k in mapList: 
+        dataDict = dataDict[k]
+    return dataDict
+
+
+# Used to update values for specific keys
+def setValueInDict(dataDict, mapList, value):
+    getFromDict(dataDict, mapList[:-1])[mapList[-1]] = value
+    return dataDict
+
+
+# Used to rename key in file
+def renameKeyInDict(dataDict, keyDict):
+    new_dict = { }
+    for key in dataDict.keys():
+        new_key = keyDict.get(key, key)
+        if isinstance(dataDict[key], dict):
+            new_dict[new_key] = renameKeyInDict(dataDict[key], keyDict)
+        else:
+            new_dict[new_key] = dataDict[key]
+    return new_dict
+
+
+def main():
+
+    directory = os.getenv('DIRECTORY')
+    yaml_file = os.getenv('YAMLFILE')
+    payload = os.getenv('PAYLOAD')
+    edit_object = os.getenv('EDIT_OBJECT')
+    yaml_path = os.path.join(directory, yaml_file)
+
+    # Open YAML file for editing
+    try:
+        with open(yaml_path) as f:
+            dataDict = yaml.safe_load(f)
+    except:
+        print('File Not Found')
+        print(yaml_path)
+        sys.exit(1)
+
+    if edit_object == 'composition':
+        # Replace version
+        key = 'version'
+        mapList = key.split('.')
+        setValueInDict(dataDict, mapList, '3.0')
+        # Remove ports
+        delKeysFromDict(dataDict, 'ports')
+    elif edit_object == 'key':
+        # Rename Keys
+        keyDict = { }
+        oldKey, NewKey = payload.split(";")
+        keyDict[oldKey] = NewKey    
+        dataDict = renameKeyInDict(dataDict, keyDict)
+    elif edit_object == 'value':
+        # Update values
+        key, value = payload.split('=')
+        mapList = key.split('.')
+        setValueInDict(dataDict, mapList, value)
+    else:
+        print(f'Edit Object: {edit_object} !Not Recognized.')
+        sys.exit(1)
+
+    # Write YAML file
+    with open(yaml_path, 'w') as f:
+        yaml.dump(dataDict, f)
+
+    # Setting indicator step completed successfully to skip compose step
+    file_env_to_export_path = '/meta/env_vars_to_export'
+
+    print("Set Environment Variable COMPOSITION_MODIFIED to true")
+    with open(file_env_to_export_path, 'a') as file:
+        file.write("COMPOSITION_MODIFIED=true")
+
+if __name__ == "__main__":
+    main()

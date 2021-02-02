25c7e7afe4dc0c01ae55b85aab1ce99ad46e4256
Laurent Rochette <laurent.rochette@codefresh.io>
2021-02-02 16:10:10 -0600

    Adding pipeline to create image

---
+++ incubating/pipeline-trigger-merge/merge.sh
@@ -0,0 +1,33 @@
+#/bin/#!/usr/bin/env bash
+
+process_dir() {
+  echo  "Processing trigger directory $1"
+  for FILE in $1/* ; do
+    process_file $FILE
+  done
+}
+
+process_file() {
+  if [ -f $1 ]; then
+    echo  "Processing trigger file $1"
+    if [ $count -eq 1 ] ; then
+      yq -s -y '.[0].spec.triggers = [.[1].spec.triggers | add] | .[0]' $curdir/$SPEC $1 > $CF_BUILD_ID.yml
+    else
+      yq -s -y '.[0].spec.triggers = [.[].spec.triggers |.[]] | .[0]' $curdir/$SPEC $1 > $CF_BUILD_ID.yml
+    fi
+    mv $CF_BUILD_ID.yml $curdir/$SPEC
+    count=$(expr $count + 1)
+    return
+  fi
+  if [ -d $1 ] ; then
+    process_dir $1
+    return
+  fi
+  echo "WARNING: Unknown trigger file $1"
+}
+export curdir=`pwd`
+export count=1
+for f in `echo $TRIGGERS` ; do
+    process_file $f
+done
+codefresh create pipeline -f $SPEC || codefresh replace -f $SPEC

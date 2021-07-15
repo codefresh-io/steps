670efc6106e4d3898cca89bb15ddf51fbf6ed2a9
Ted Spinks <ted.spinks@codefresh.io>
2021-07-14 23:18:01 -0400

    Test and fix update-chart-py bugs through update_dependency_list()

--- incubating/update-chart/update-chart.py
+++ incubating/update-chart/update-chart.py
@@ -15,16 +15,19 @@ import yaml
 
 
 def str_to_bool(bool_string):
+    """ Convert a 'true' or 'false' string to a boolean.
+    """
     assert bool_string.lower() in ['true', 'false'], "Value '{}' cannot be converted to boolean".format(bool_string)
     if bool_string.lower() == "true":
         return True
     return False
 
 def get_inputs_from_env_vars():
-    """ Read environment variables. Format variables into lists and dictionaries to match the corresponding
-        Chart.yaml structure.
+    """ Read environment variables. Format variables into lists and dictionaries to match their corresponding
+        Chart.yaml structure. See step.yaml for full descriptions of environment variables.
     """
-    increment_chart_version = os.environ.get("INCREMENT_CHART_VERSION")
+    increment_chart_version = str_to_bool(os.environ.get("INCREMENT_CHART_VERSION"))
+    print(increment_chart_version)
     values_to_update = os.environ.get("VALUES_TO_UPDATE")
     if values_to_update:
         values_to_update = values_to_update.split(',')
@@ -39,36 +42,37 @@ def get_inputs_from_env_vars():
     if sources:
         sources = sources.split(',')
     sources_replace = str_to_bool(os.environ.get("SOURCES_REPLACE"))
-    dependency_replace = str_to_bool(os.environ.get("DEPENDENCY_REPLACE"))
+    dependency_match_alias = str_to_bool(os.environ.get("DEPENDENCY_MATCH_ALIAS"))
     dependency_name = os.environ.get("DEPENDENCY_NAME")
-    # Create a dict to store 'dependencies' entries to update
-    dependency_chart={}
+    dependency_tags_replace = str_to_bool(os.environ.get("DEPENDENCY_TAGS_REPLACE"))
+    dependency_import_values_replace = str_to_bool(os.environ.get("DEPENDENCY_IMPORT_VALUES_REPLACE"))
+    # Create a dict to store the 'dependencies' chart fields to update
+    updated_dependency_fields={}
     if dependency_name:
-        dependency_chart['name'] = dependency_name
+        updated_dependency_fields['name'] = dependency_name
         dependency_version = os.environ.get("DEPENDENCY_VERSION")
         if dependency_version:
-            dependency_chart['version'] = dependency_version
+            updated_dependency_fields['version'] = dependency_version
         dependency_repository = os.environ.get("DEPENDENCY_REPOSITORY")
         if dependency_repository:
-            dependency_chart['repository'] = dependency_repository
+            updated_dependency_fields['repository'] = dependency_repository
         dependency_condition = os.environ.get("DEPENDENCY_CONDITION")
         if dependency_condition:
-            dependency_chart['condition'] = dependency_condition
+            updated_dependency_fields['condition'] = dependency_condition
         dependency_tags = os.environ.get("DEPENDENCY_TAGS")
         if dependency_tags:
-            dependency_chart['tags'] = dependency_tags.split(',')
-        dependency_tags_replace = str_to_bool(os.environ.get("DEPENDENCY_TAGS_REPLACE"))
+            updated_dependency_fields['tags'] = dependency_tags.split(',')
         dependency_import_values = os.environ.get("DEPENDENCY_IMPORT_VALUES")
         if dependency_import_values:
-            dependency_chart['import-values'] = dependency_import_values.split(',')
-        dependency_import_values_replace = str_to_bool(os.environ.get("DEPENDENCY_IMPORT_VALUES_REPLACE"))
+            updated_dependency_fields['import-values'] = dependency_import_values.split(',')
         dependency_alias = os.environ.get("DEPENDENCY_ALIAS")
         if dependency_alias:
-            dependency_chart['alias'] = dependency_alias
+            updated_dependency_fields['alias'] = dependency_alias
     annotations = os.environ.get("ANNOTATIONS")
     annotations_replace = str_to_bool(os.environ.get("ANNOTATIONS_REPLACE"))
-    return increment_chart_version, values_to_update, fields_to_update, keywords, keywords_replace, sources, sources_replace, \
-        dependency_chart, dependency_tags_replace, dependency_import_values_replace, annotations, annotations_replace
+    return increment_chart_version, values_to_update, fields_to_update, keywords, keywords_replace, sources, \
+        sources_replace, updated_dependency_fields, dependency_match_alias, dependency_tags_replace, \
+        dependency_import_values_replace, annotations, annotations_replace
 
 
 def increment_version(version):
@@ -88,9 +92,11 @@ def increment_version(version):
 
 
 def update_chart_fields(chart_yaml, increment_chart_version, fields_to_update):
-    """ Take each "key=value" string in fields_to_update list, and apply it to chart_yaml dict. If a
-        'version' wasn't provided, then increment the 'version' based on increment_chart_version bool.
+    """ Take each "key=value" string in fields_to_update list, and apply them to chart_yaml as top-level key:value
+        pairs. If 'version' isn't in the list, then increment the 'version' according to increment_chart_version bool.
     """
+    if not fields_to_update:
+        fields_to_update = []
     explicit_version = False
     for field_str in fields_to_update:
         key_value_pair = field_str.split('=')
@@ -99,20 +105,94 @@ def update_chart_fields(chart_yaml, increment_chart_version, fields_to_update):
         if key == "version":
             explicit_version = True
         chart_yaml[key] = value
-    if not explicit_version and increment_chart_version:
+    if (not explicit_version) and (increment_chart_version):
         chart_yaml['version'] = increment_version(chart_yaml['version'])
 
 
-def update_keywords_list(chart_yaml, keywords, keywords_replace):
-    pass
+def update_list(chart_yaml, list_name, new_list, replace_flag):
+    """ Add the elements from new_list to the specified list in chart_yaml. If replace_flag
+        is True then don't bother appending them, just overwrite the whole list.
+    """
+    if not new_list:
+        return
+    # Overwrite if replace_flag, or if the list doesn't exist in chart_yaml yet
+    if (replace_flag) or (list_name not in chart_yaml.keys()) or (not isinstance(chart_yaml[list_name], list)):
+        chart_yaml[list_name] = new_list
+    else:
+        for element_to_add in new_list:
+            # Don't add duplicate elements
+            if element_to_add not in chart_yaml[list_name]:
+                chart_yaml[list_name].append(element_to_add)
 
 
-def update_sources_list(chart_yaml, sources, sources_replace):
-    pass
+def get_dependency_by_name_and_alias(chart_yaml, chart_name, alias):
+    """ Get a chart from the 'dependencies' list in chart_yaml by its name and alias. If the specified
+        chart does not exist in the list, return None.
+    """
+    for dependency in chart_yaml['dependencies']:
+        if 'name' in dependency.keys() and dependency['name'] == chart_name:
+            if 'alias' in dependency.keys() and dependency['alias'] == alias:
+                return dependency
+    return None
 
 
-def update_dependency_list(chart_yaml, dependency_chart, dependency_tags_replace, dependency_import_values_replace):
-    pass
+def get_or_create_dependency_by_name(chart_yaml, chart_name):
+    """ Get a chart from the 'dependencies' list in chart_yaml by its name. If it doesn't exist yet,
+        then add a new chart to the list with that name, and return that.
+    """
+    if 'dependencies' not in chart_yaml.keys():
+        chart_yaml['dependencies'] = []
+    for dependency in chart_yaml['dependencies']:
+        if 'name' in dependency.keys() and dependency['name'] == chart_name:
+            return dependency
+    new_dependency = { "name": chart_name }
+    chart_yaml['dependencies'].append(new_dependency)
+    return new_dependency
+
+
+def update_dependency_list(chart_yaml, updated_dependency_fields, dependency_match_alias, 
+                           dependency_tags_replace, dependency_import_values_replace):
+    """ Updates a chart in chart_yaml's 'dependencies' list. The chart to update is specified by its 'name' and 
+        optionally also its 'alias' depending on the dependency_match_alias bool param. When updating the chart, its 
+        list fields can either be appended or replaced, depending on their bool params.
+    """
+    # ******** Stop now if there is nothing to do ********
+    if not updated_dependency_fields:
+        return
+    # ******** Get the chart to update *********
+    if 'dependencies' not in chart_yaml.keys():
+        chart_yaml['dependencies'] = []
+    chart_name = updated_dependency_fields['name']
+    chart = None
+    alias = None
+    if 'alias' in updated_dependency_fields.keys():
+        alias = updated_dependency_fields['alias']
+    if alias and dependency_match_alias:
+        chart = get_dependency_by_name_and_alias(chart_yaml, chart_name, alias)
+    if not chart:
+        chart = get_or_create_dependency_by_name(chart_yaml, chart_name)
+    # ******** Copy updated fields into the chart *********
+    for key in updated_dependency_fields.keys():
+        # Append the 'tags' list if specified
+        if key == 'tags' and not dependency_tags_replace:
+            if 'tags' not in chart.keys():
+                chart['tags'] = []
+            for tag in updated_dependency_fields['tags']:
+                # Don't add duplicate elements
+                if tag not in chart['tags']:
+                    chart['tags'].append(tag)
+            continue
+        # Append the 'import-values' list if specified
+        if key == 'import-values' and not dependency_import_values_replace:
+            if 'import-values' not in chart.keys():
+                chart['import-values'] = []
+            for import_value in updated_dependency_fields['import-values']:
+                # Don't add duplicate elements
+                if import_value not in chart['import-values']:
+                    chart['import-values'].append(import_value)
+            continue
+        # Copy the field exactly / replace whole list
+        chart[key] = updated_dependency_fields[key]   
 
 
 def update_annotations_dict(chart_yaml, annotations, annotations_replace):
@@ -125,24 +205,30 @@ def update_values(values_yaml, values_to_update):
 
 def main():
     # Get script args
-    increment_chart_version, values_to_update, fields_to_update, keywords, keywords_replace, sources, sources_replace, dependency_chart, \
-        dependency_tags_replace, dependency_import_values_replace, annotations, annotations_replace = get_inputs_from_env_vars()
+    increment_chart_version, values_to_update, fields_to_update, keywords, keywords_replace, sources, \
+         sources_replace, updated_dependency_fields, dependency_match_alias, dependency_tags_replace, \
+         dependency_import_values_replace, annotations, annotations_replace = get_inputs_from_env_vars()
     
     # Read source YAML files
     with open("Chart.yaml", "r") as read_file:
         chart_yaml = yaml.load(read_file, Loader=yaml.SafeLoader)
-    with open("values.yaml", "r") as read_file:
-        values_yaml = yaml.load(read_file, Loader=yaml.SafeLoader)
+    # with open("values.yaml", "r") as read_file:
+    #     values_yaml = yaml.load(read_file, Loader=yaml.SafeLoader)
 
     # Update chart_yaml
     update_chart_fields(chart_yaml, increment_chart_version, fields_to_update)
-    update_keywords_list(chart_yaml, keywords, keywords_replace)
-    update_sources_list(chart_yaml, sources, sources_replace)
-    update_dependency_list(chart_yaml, dependency_chart, dependency_tags_replace, dependency_import_values_replace)
-    update_annotations_dict(chart_yaml, annotations, annotations_replace)
+    update_list(chart_yaml, 'keywords', keywords, keywords_replace)
+    update_list(chart_yaml, 'sources', sources, sources_replace)
+    update_dependency_list(chart_yaml, updated_dependency_fields, dependency_match_alias, dependency_tags_replace, dependency_import_values_replace)
+    # update_annotations_dict(chart_yaml, annotations, annotations_replace)
+    
+    # Output chart_yaml
+    print(chart_yaml)
+    with open("Chart.yaml", "w") as write_file:
+        document = yaml.dump(chart_yaml, write_file)
    
     # Update values_yaml
-    update_values(values_yaml, values_to_update)
+    # update_values(values_yaml, values_to_update)
 
 
 if __name__ == "__main__":

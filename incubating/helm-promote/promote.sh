e94bd927807b972eb5686fa954f35b923cccde27
Jesse Antoszyk <jesse@antoszyk.com>
2021-09-09 10:22:00 -0400

    Add debug options

--- incubating/helm-promote/promote.sh
+++ incubating/helm-promote/promote.sh
@@ -26,6 +26,20 @@ TO_VALUES_YAML="${TO_VALUES_YAML:-${VALUES_YAML}}"
 # If chart names differ from image names, they can be set independently
 PROMOTE_SUBCHARTS="${PROMOTE_SUBCHARTS:-${PROMOTE_IMAGES}}"
 
+DEBUG="${DEBUG:-false}"
+
+# Info printer
+function print_info() {
+  echo -e "INFO: $1"
+}
+
+# Debug printer
+function print_debug() {
+  if [[ "${DEBUG}" == "true" ]]; then
+    echo -e "DEBUG: $1"
+  fi
+}
+
 # Error formatter
 function print_error() {
   echo >&2 -e "ERROR: $1"; exit "$2" || exit 1;
@@ -35,9 +49,11 @@ function update_chart_yaml() {
   # Not strictly needed, but more explicit
   local CHART=$1
 
-  echo "INFO: Updating dependency in ${PROMOTE_TO}/${TO_CHART_YAML}"
+  print_info "Updating dependency ${CHART} in ${PROMOTE_TO}/${TO_CHART_YAML}"
   # Get the new version of the chart
   local PROMOTED_CHART_VERSION=$(yq eval '.dependencies[] | select (.name == "'${CHART}'") | .version' "${PROMOTE_FROM}/${FROM_CHART_YAML}")
+
+  print_debug "PROMOTED_CHART_VERSION: ${PROMOTED_CHART_VERSION}"
   # Update Chart.y[a]ml to the new chart version
   yq eval --inplace '.dependencies[] |= select (.name == "'${CHART}'") .version="'${PROMOTED_CHART_VERSION}'"' "${PROMOTE_TO}/${TO_CHART_YAML}"
 }
@@ -46,7 +62,7 @@ function update_values_yaml() {
   # Not strictly needed, but more explicit
   local IMAGE=$1
 
-  echo "INFO: Updating value in ${PROMOTE_TO}/${TO_VALUES_YAML}"
+  print_info "Updating value for ${IMAGE} in ${PROMOTE_TO}/${TO_VALUES_YAML}"
   # Expand variables in replace string, potentially unsafe -- consider replacing
   local REPLACE_STR=$(echo ${IMAGE_FORMAT} | sed "s/{{.*}}/${IMAGE}/g")
   # Check if first char is a dot if not, make it one
@@ -54,8 +70,11 @@ function update_values_yaml() {
     REPLACE_STR=".${REPLACE_STR}"
   fi
 
+  print_debug "REPLACE_STR: ${REPLACE_STR}"
   # IF YAML formats are not the same between PROMOTE_FROM and PROMOTE_TO this will fail
   local PROMOTED_IMAGE_VERSION=$(yq eval ${REPLACE_STR} "${PROMOTE_FROM}/${FROM_VALUES_YAML}")
+
+  print_debug "PROMOTED_IMAGE_VERSION: ${PROMOTED_IMAGE_VERSION}"
   yq eval --inplace ${REPLACE_STR}'="'${PROMOTED_IMAGE_VERSION}'"' "${PROMOTE_TO}/${TO_VALUES_YAML}"
 }
 
@@ -80,6 +99,7 @@ bump_semver() {
     ;;
   esac
 
+  # Return value
   echo "${MAJOR}.${MINOR}.${PATCH}"
 }
 
@@ -93,10 +113,11 @@ update_chart_version() {
   # Patch chart version
   yq eval --inplace '.version="'${NEW_VERSION}'"' "${PROMOTE_TO}/${TO_CHART_YAML}"
 
-  echo "INFO: Bumped chart version from ${BASE_VERSION} to ${NEW_VERSION}"
+  print_info "Bumped chart version from ${BASE_VERSION} to ${NEW_VERSION}"
 }
 
 function nonzero_checks() {
+  print_debug "Running input checks"
   # Run some checks to make sure required parameters exist
   # Make sure vars not empty
   if [[ -z "${PROMOTE_FROM}" ||  -z "${PROMOTE_TO}" ]]; then
@@ -104,10 +125,8 @@ function nonzero_checks() {
   fi
 
   # Make sure vars not equal
-  if [[ "${PROMOTE_FROM}" == "${PROMOTE_TO}" ]]; then
-    if [[ "${{FROM_VALUES_YAML}}" == "${{TO_VALUES_YAML}}" ]]; then
-        print_error "PROMOTE_FROM and PROMOTE_TO variables cannot be equal if FROM_VALUES_YAML is equal to TO_VALUES_YAML" 4
-    fi
+  if [[ "${PROMOTE_FROM}" == "${PROMOTE_TO}" ]] && [[ "${FROM_VALUES_YAML}" == "${TO_VALUES_YAML}" ]]; then
+    print_error "PROMOTE_FROM and PROMOTE_TO variables cannot be equal if FROM_VALUES_YAML is equal to TO_VALUES_YAML" 4
   fi
   # Make sure dirs exist
   stat ${PROMOTE_FROM} &> /dev/null && stat ${PROMOTE_TO} &> /dev/null || (print_error "PROMOTE_FROM and PROMOTE_TO directories must both exist" 8)
@@ -118,8 +137,10 @@ main() {
 
   # Update the chart version
   if [[ "${BUMP_VERSION}" == "true" ]]; then
-      update_chart_version
-    fi
+    update_chart_version
+  else
+    print_debug "Skipping bump version"
+  fi
 
   # These are in a loops -- lots of writes to the same file if updating multiple items :/ Consider a way to unravel
   # ---- UPDATE VALUES.YAML ----
@@ -133,4 +154,19 @@ main() {
   done
 }
 
+print_debug "Debug mode ON"
+print_debug "\
+    \n\tPROMOTE_FROM: '${PROMOTE_FROM}'\
+    \n\tPROMOTE_TO: '${PROMOTE_TO}'\
+    \n\tBUMP_VERSION_TYPE: '${BUMP_VERSION_TYPE}'\
+    \n\tPROMOTE_IMAGES: '${PROMOTE_IMAGES}'\
+    \n\tPROMOTE_SUBCHARTS: '${PROMOTE_SUBCHARTS}'\
+    \n\tIMAGE_FORMAT: '${IMAGE_FORMAT}'\
+    \n\tVALUES_YAML: '${VALUES_YAML}'\
+    \n\tFROM_VALUES_YAML: '${FROM_VALUES_YAML}'\
+    \n\tTO_VALUES_YAML: '${TO_VALUES_YAML}'\
+    \n\tCHART_YAML: '${CHART_YAML}'\
+    \n\tFROM_CHART_YAML: '${FROM_CHART_YAML}'\
+    \n\tTO_CHART_YAML: '${TO_CHART_YAML}'\
+    "
 main

5b7e211de74cb1294634276af791709f0c7f613a
Laurent Rochette <laurent.rochette@codefresh.io>
2023-04-10 16:49:27 -0700

    Fixing some syntax issues Remove subfolder in S3 bucket for build ID Adding IMAGE NAME and VERSION for easier testing Make script executable Make non public Add parameter for integration name
    Signed-off-by: Laurent Rochette <laurent.rochette@codefresh.io>

--- incubating/aws-s3/src/start.sh
+++ incubating/aws-s3/src/start.sh
@@ -1,12 +1,12 @@
-AWS_ACCESS_KEY_ID=$(codefresh get context amazon --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.accessKeyId')
-AWS_SECRET_ACCESS_KEY=$(codefresh get context amazon --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.secretAccessKey')
+AWS_ACCESS_KEY_ID=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.accessKeyId')
+AWS_SECRET_ACCESS_KEY=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.secretAccessKey')
 AWS_DEFAULT_REGION=$(echo "${REGION,,}")
 AWS_PAGER=""
 KEY=$(echo $S3PREFIX/$CF_BUILD_ID)
 
 cd $working_directory
 
-aws s3 cp $SOURCE s3://$BUCKET/$KEY --recursive
+aws s3 cp $SOURCE s3://$BUCKET/$S3PREFIX --recursive
 
 export CF_OUTPUT_URL="https://s3.console.aws.amazon.com/s3/buckets/$BUCKET?region=$AWS_DEFAULT_REGION&prefix=$KEY/"
 

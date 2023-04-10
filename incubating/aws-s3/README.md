e8d8689fc3cdc050846a193b7047b6e9ca7c6892
Luke Goodfellow <107487942+mnmlluke@users.noreply.github.com>
2023-04-10 14:34:34 -0700

    Created initial s3 step

---
+++ incubating/aws-s3/README.md
@@ -0,0 +1,1 @@
+# cf-plugin-s3
---
+++ incubating/aws-s3/src/start.sh
@@ -0,0 +1,15 @@
+AWS_ACCESS_KEY_ID=$(codefresh get context amazon --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.accessKeyId')
+AWS_SECRET_ACCESS_KEY=$(codefresh get context amazon --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.secretAccessKey')
+AWS_DEFAULT_REGION=$(echo "${REGION,,}")
+AWS_PAGER=""
+KEY=$(echo $S3PREFIX/$CF_BUILD_ID)
+
+cd $working_directory
+
+aws s3 cp $SOURCE s3://$BUCKET/$KEY --recursive
+
+export CF_OUTPUT_URL="https://s3.console.aws.amazon.com/s3/buckets/$BUCKET?region=$AWS_DEFAULT_REGION&prefix=$KEY/"
+
+echo "Files have been upload to: \n\n$CF_OUTPUT_URL \n"
+
+cf_export CF_OUTPUT_URL

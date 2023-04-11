490cc1edb037034595998ac4f336f4fd15d72210
Luke Goodfellow <107487942+mnmlluke@users.noreply.github.com>
2023-04-11 12:45:43 -0700

    Reduced image size, update step, updaet script

--- incubating/aws-s3/src/start.sh
+++ incubating/aws-s3/src/start.sh
@@ -2,19 +2,25 @@ export AWS_ACCESS_KEY_ID=$(codefresh get context ${S3_INTEGRATION} --decrypt -o 
 export AWS_SECRET_ACCESS_KEY=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.secretAccessKey')
 export AWS_DEFAULT_REGION=$(echo "${REGION,,}")
 export AWS_PAGER=""
+
 cd $working_directory
 echo "Starting to upload files to S3"
 echo ""
-if [ -d $SOURCEDIR ]; then
-  aws s3 cp $SOURCEDIR s3://$BUCKET/$S3_PREFIX --recursive
+
+if [ -d $SOURCE ]; then
+  aws s3 cp $SOURCE s3://$BUCKET/$S3_PREFIX --recursive
 else
-  aws s3 cp $SOURCEDIR s3://$BUCKET/$S3_PREFIX
+  aws s3 cp $SOURCE s3://$BUCKET/$S3_PREFIX
 fi
+
 echo ""
 echo "Finished uplaoding files to S3"
 echo ""
+
 export uploadToS3_CF_OUTPUT_URL="https://s3.console.aws.amazon.com/s3/buckets/$BUCKET?region=$AWS_DEFAULT_REGION&prefix=$S3_PREFIX/"
+
 echo "Files have been upload to: "
 echo $uploadToS3_CF_OUTPUT_URL
 echo ""
+
 cf_export uploadToS3_CF_OUTPUT_URL

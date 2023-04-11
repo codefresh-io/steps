d28ffb2ea8477d2b7b063731aad1e318d7fbf214
Luke Goodfellow <107487942+mnmlluke@users.noreply.github.com>
2023-04-11 08:25:23 -0700

    fixed aws export commands, output url, cf api key

--- incubating/aws-s3/src/start.sh
+++ incubating/aws-s3/src/start.sh
@@ -1,15 +1,15 @@
-AWS_ACCESS_KEY_ID=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.accessKeyId')
-AWS_SECRET_ACCESS_KEY=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.secretAccessKey')
-AWS_DEFAULT_REGION=$(echo "${REGION,,}")
-AWS_PAGER=""
-KEY=$(echo $S3PREFIX/$CF_BUILD_ID)
+export AWS_ACCESS_KEY_ID=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.accessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.secretAccessKey')
+export AWS_DEFAULT_REGION=$(echo "${REGION,,}")
+export AWS_PAGER=""
 
 cd $working_directory
 
-aws s3 cp $SOURCE s3://$BUCKET/$S3PREFIX --recursive
+aws s3 cp $SOURCEDIR s3://$BUCKET/$S3_PREFIX --recursive
 
-export CF_OUTPUT_URL="https://s3.console.aws.amazon.com/s3/buckets/$BUCKET?region=$AWS_DEFAULT_REGION&prefix=$KEY/"
+export uploadToS3_CF_OUTPUT_URL="https://s3.console.aws.amazon.com/s3/buckets/$BUCKET?region=$AWS_DEFAULT_REGION&prefix=$S3_PREFIX/"
 
-echo "Files have been upload to: \n\n$CF_OUTPUT_URL \n"
+echo "Files have been upload to: "
+echo $uploadToS3_CF_OUTPUT_URL
 
-cf_export CF_OUTPUT_URL
+cf_export uploadToS3_CF_OUTPUT_URL

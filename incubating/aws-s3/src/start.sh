68337a02d5f518df1f6884e15e74e53e5011c5d9
Luke Goodfellow <107487942+mnmlluke@users.noreply.github.com>
2023-04-12 13:21:49 -0700

    AWS-S3 step (#572)
    * Created initial s3 step
    
    * Fixing some syntax issues
    Remove subfolder in S3 bucket for build ID
    Adding IMAGE NAME and VERSION for easier testing
    Make script executable
    Make non public
    Add parameter for integration name
    
    Signed-off-by: Laurent Rochette <laurent.rochette@codefresh.io>
    
    * fixed aws export commands, output url, cf api key
    
    * updated working Dir and script
    
    * updated steps.yaml for public
    
    * upated steps.yaml to rquire sourcedir
    
    * adding test for directory
    
    Signed-off-by: Laurent Rochette <laurent.rochette@codefresh.io>
    
    * Reduced image size, update step, updaet script
    
    * updated steps.yaml with corrections
    
    * updated URL to image
    
    ---------
    
    Signed-off-by: Laurent Rochette <laurent.rochette@codefresh.io>
    Co-authored-by: Laurent Rochette <laurent.rochette@codefresh.io>

---
+++ incubating/aws-s3/src/start.sh
@@ -0,0 +1,26 @@
+export AWS_ACCESS_KEY_ID=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.accessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(codefresh get context ${S3_INTEGRATION} --decrypt -o json |jq --raw-output '.spec.data.auth.jsonConfig.secretAccessKey')
+export AWS_DEFAULT_REGION=$(echo "${REGION,,}")
+export AWS_PAGER=""
+
+cd $working_directory
+echo "Starting to upload files to S3"
+echo ""
+
+if [ -d $SOURCE ]; then
+  aws s3 cp $SOURCE s3://$BUCKET/$S3_PREFIX --recursive
+else
+  aws s3 cp $SOURCE s3://$BUCKET/$S3_PREFIX
+fi
+
+echo ""
+echo "Finished uplaoding files to S3"
+echo ""
+
+export uploadToS3_CF_OUTPUT_URL="https://s3.console.aws.amazon.com/s3/buckets/$BUCKET?region=$AWS_DEFAULT_REGION&prefix=$S3_PREFIX/"
+
+echo "Files have been upload to: "
+echo $uploadToS3_CF_OUTPUT_URL
+echo ""
+
+cf_export uploadToS3_CF_OUTPUT_URL

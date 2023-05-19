af58884a1e58587db8096f681d44b8686325859e
Laurent Rochette <laurent.rochette@codefresh.io>
2023-05-19 13:58:57 -0700

    Using /meta to export var

--- incubating/argo-cd-sync/argocd_sync.py
+++ incubating/argo-cd-sync/argocd_sync.py
@@ -82,10 +82,11 @@ def execute_argocd_sync(ingress_host):
 
 
 def export_variable(var_name, var_value):
-    path = os.getenv('CF_VOLUME_PATH') if os.getenv('CF_VOLUME_PATH') != None else './'
+    #path = os.getenv('CF_VOLUME_PATH') if os.getenv('CF_VOLUME_PATH') != None else './'
+    path = "/meta";
     with open(path+'/env_vars_to_export', 'a') as a_writer:
         a_writer.write(var_name + "=" + var_value+'\n')
-    print("Exporting variable: "+var_name+"="+var_value)  
+    print("Exporting variable: "+var_name+"="+var_value)
 
 ##############################################################
 
